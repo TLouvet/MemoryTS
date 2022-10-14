8c0ae98865531718cddedbbd2f38f7842f5ce352
Louvet Thomas <thomas.louvet@laposte.net>
2022-10-14 22:11:13 +0200

    removing dist from git track

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WAIT_TIME = exports.CARDS_NUMBERS = void 0;
-// options for the number of cards to find by the player
-exports.CARDS_NUMBERS = [2, 4, 6, 8, 10, 12, 14, 16];
-// delay applied when two cards are unfolded
-exports.WAIT_TIME = 750;
